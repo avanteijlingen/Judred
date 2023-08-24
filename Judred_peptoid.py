ec8757cc71ac8666becb7153f4fea8577d0489b8
avanteijlingen <57176642+avanteijlingen@users.noreply.github.com>
2023-08-24 13:49:54 +0100

    Update Judred_peptoid.py

--- Judred_peptoid.py
+++ Judred_peptoid.py
@@ -36,10 +36,28 @@ Num2Word = {1:"AminoAcids",
             7:"Hepta",
             8:"Octa"}
 
-L = int(sys.argv[1])
-steps = np.array([20**i for i in range(L-1, -1, -1)], dtype=np.uint64)
+# =============================================================================
+# L = int(sys.argv[1])
+# steps = np.array([20**i for i in range(L-1, -1, -1)], dtype=np.uint64)
+# 
+# fname = Num2Word[L].lower()+"peptides_normalized.parquet"
+# #fname_memmap = Num2Word[L].lower()+"peptides_indexes.memmap"
+# =============================================================================
+# Nfex = 'Nfer'/'Nfes',
+letters_1 = np.array(['Na', 'Nab', 'Nd', 'Ne', 'Nf', 'Nfe', 'NfeBr', 'NfeCl', "Nfex", 'Nfn', 'Nfnap', 'Nfp', 'Ni', 'Nk', 'Nke', 'Nkeqm', 'Nl', 'Nm', 'NmO', 'Nn', 'Nq', 'Nr', 'Ns', 'Nse', 'Nt', 'Nv', 'Nw', 'Nwe', 'Ny'])
 
-fname = Num2Word[L].lower()+"peptides_normalized.parquet"
-#fname_memmap = Num2Word[L].lower()+"peptides_indexes.memmap"
-
-letters_1 = np.array(["A", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "Y"])
+for peptoid in letters_1:
+    if peptoid == "Nfex":
+        peptoid_folder = "Nfes"
+    else:
+        peptoid_folder = peptoid
+    
+    for state in ['gas', 'octanol', 'water']:
+        folder = f"peptoid_data/{peptoid_folder}/{state}"
+        for file in os.listdir(folder):
+            path = f"{folder}/{file}"
+            op = orca_parser.ORCAParse(path)
+            print(op.valid)
+    
+    
+    break
